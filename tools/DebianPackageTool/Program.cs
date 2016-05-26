5fe706c65498308bc1c004b6dd2dd9816fd625f4
Bryan Thornbury <brthor@microsoft.com>
2016-05-26 11:32:04 -0700

    dotnet-deb-tool

---
+++ tools/DebianPackageTool/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Reflection;
+
+namespace Dotnet.Deb.Tool
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var processInfo = new ProcessStartInfo()
+            {
+                FileName = Path.Combine(typeof(Program).GetTypeInfo().Assembly.Location, "tool", "package_tool"),
+                Arguments = ArgumentEscaper.EscapeAndConcatenateArgArrayForProcessStart(args),
+                UseShellExecute = false
+            };
+
+            var process = new Process()
+            {
+                StartInfo = processInfo
+            };
+
+            process.Start();
+            process.WaitForExit();
+        }
+    }
+}
